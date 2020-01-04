5f0f0698aee257fed09fd745e4724016dd685e95
Bogdan Mihai <bogdan-eugen.mihai@klarna.com>
2020-01-04 01:50:57 +0100

    Add some basic tests

---
+++ hello-aws-lambda-python3.8/tests/fn_hello_tests.py
@@ -0,0 +1,37 @@
+import unittest
+
+from app import fn_hello
+from app.fn_hello import SimpleException
+
+
+class TestProcessRequest(unittest.TestCase):
+
+    def test_process_request_no_error(self):
+
+        event = {
+            "first_name": "Bogdan-Eugen",
+            "last_name": "Mihai",
+            "age": 34,
+            "throw_error": False
+        }
+        response = fn_hello.process_request(event, "pip")
+
+        self.assertEqual("Hello Bogdan-Eugen Mihai, this is a Python3.8 AWS Lambda function developed using pip!",
+                         response["greeting"])
+        self.assertEqual("Your age in hexadecimal is 0x22", response["result"])
+
+    def test_process_request_throw_error(self):
+
+        event = {
+            "first_name": "Bogdan-Eugen",
+            "last_name": "Mihai",
+            "age": 34,
+            "throw_error": True
+        }
+
+        with self.assertRaises(SimpleException):
+            fn_hello.process_request(event, "pip")
+
+
+if __name__ == '__main__':
+    unittest.main()
